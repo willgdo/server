1f4d9afede813ea1712f61be47d22e4a029aa176
Willian Oliveira <willian.oliveira737@gmail.com>
2025-04-23 01:01:49 -0300

    Separando parametros

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,5 +1,8 @@
 import { routes } from "../routes.js";
-import { extractQueryParams } from "../utils/extract-queue-params.js";
+import { extractQueryParams } from "../utils/extract-query-params.js";
+import { Database } from "../databases.js";
+
+const database = new Database();
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
@@ -13,7 +16,7 @@ export function routeHandler(request, response) {
     request.params = params;
     request.query = query ? extractQueryParams(query) : {};
 
-    return route.controller(request, response);
+    return route.controller({ request, response, database });
   }
 
   return response.writeHead(404).end("Rota não encontrada!");
--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,26 @@ export const routes = [
   {
     method: "GET",
     path: "/products",
-    controller: (request, response) => {
-      return response.end(JSON.stringify(request.query));
+    controller: ({ request, response, database }) => {
+      const products = database.select("products");
+
+      return response.end(JSON.stringify(products));
     },
   },
   {
     method: "POST",
     path: "/products",
-    controller: (request, response) => {
-      return response.writeHead(201).end(JSON.stringify(request.body));
+    controller: ({ request, response, database }) => {
+      const { name, price } = request.body;
+
+      database.insert("products", { name, price });
+      return response.writeHead(201).end();
     },
   },
   {
     method: "DELETE",
     path: "/products/:id",
-    controller: (request, response) => {
+    controller: ({ request, response }) => {
       return response.end("Produto removido com ID: " + request.params.id);
     },
   },
